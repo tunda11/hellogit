8ae718be82f3542a1fe72a27a534540f0ca3d666
Guest <tunda11@users.noreply.github.com>
2017-09-13 15:41:53 -0400

    Assignment.cpp

---
+++ Assignment.cpp
@@ -0,0 +1,18 @@
+//client 
+
+int main ()
+	{	Birthday bDay;
+		bDay.seBirth( int d, string m, int y);
+		
+		vector <Birthday> v1 = {
+	{b1.setDay(), "b1.setMonth()", b1.setYear()},
+	{b2.setDay(), "b2.setMonth()", b2.setYear()},
+	{b3.setDay(), "b3.setMonth()", b3.setYear()},
+	{b4.setDay(), "b4.setMonth()", b4.setYear()},
+	};
+	 v1 = {b1, b2, b3, b4}
+	 cout<<b1.getDay()<<" "<<b1.setMonth()<< " "<<b1.getYear()<<endl;
+	 cout<<b2.getDay()<<" "<<b2.setMonth()<< " "<<b2.getYear()<<endl;
+	 cout<<b3.getDay()<<" "<<b3.setMonth()<< " "<<b3.getYear()<<endl;
+	 cout<<b4.getDay()<<" "<<b4.setMonth()<< " "<<b4.getYear()<<endl;
+	}//End of Main
