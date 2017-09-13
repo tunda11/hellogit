649aeeeb940476961da53a8d4f8a4898f0b655a8
Guest <tunda11@users.noreply.github.com>
2017-09-13 16:49:36 -0400

    Assignment.h

--- Assignment.cpp
+++ Assignment.cpp
@@ -10,15 +10,20 @@ int main ()
 		
 		
 		vector <Birthday> v1 = {
-			{12, "Nov", 1996}
-	//{bDay.setDay(5), bDay.setMonth("Nov"), bDay.setYear(97)},
+			{12, "Nov", 1996},
+			{9, "Aug", 1727},
+			{21, "Mar", 1897},
+			{15, "Apr", 1937},
+	//{bDay.setDay(5), bDay.setMonth("Nov"), bDay.setYear(97)}
 	//{bDay.setDay(1), bDay.setMonth("Dec"), bDay.setYear(95)},
 	//{bDay.setDay(2), bDay.setMonth("Jan"), bDay.setYear(98)},
 	//{bDay.setDay(3), bDay.setMonth("Feb"), bDay.setYear(99)},
 	};
-	 
-	 cout << "Done";
-	 cout << bDay.getDay() << " " << bDay.getMonth()<< " " << bDay.getYear() << endl;
+	 v1.push_back({14, "Aug", 96});
+	 for ( int p = 0; p < v1.size(); p++)
+	 {
+	 cout << v1[p].getDay() << " " << v1[p].getMonth()<< " " << v1[p].getYear() << endl;
+ }
 	 //cout << bDay.getDay() << " " << bDay.getMonth()<< " " << bDay.getYear() << endl;
 	 //cout << bDay.getDay() << " " << bDay.getMonth()<< " " << bDay.getYear() << endl;
 	 //cout << bDay.getDay() << " " << bDay.getMonth()<< " " << bDay.getYear() << endl;
